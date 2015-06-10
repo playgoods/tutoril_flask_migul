e5b7890f05dda4b7606beba2224c5d964262cd7d
mohamed ali <mohmed_e@hotmail.com>
2015-06-11 00:14:21 +0300

    modified hello.py add script manger

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask.ext.script import Manager
 
 app = Flask(__name__)
 
+manger = Manager(app)
+
 @app.route('/')
 def index():
-	return "hello world"
+	return "hello world good time who are you "
 	
 if __name__ == "__main__":
-	app.run()
+	manger.run()
