3d866a13b57e1bc2705af83e2758e2719f38090f
playgoods <mohmed_e@hotmail.com>
2015-09-03 21:28:02 +0300

    add bootstarap to appliction

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manger = Manager(app)
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
 def index():
