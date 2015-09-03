ebb3bf770ff16e32183432ec25c29a5fe123d8f2
playgoods <mohmed_e@hotmail.com>
2015-09-03 19:22:22 +0300

    add new base.html

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@ def index():
 	
 @app.route('/user/<name>')
 def user(name):
-	return render_template("user.html",name=name)
+	return render_template("user.html",name = name)
 	
 if __name__ == "__main__":
 	manger.run()
