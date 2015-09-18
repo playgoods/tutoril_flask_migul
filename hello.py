3953d3175994f02362900c7cf7114275531dbdc4
playgoods <mohmed_e@hotmail.com>
2015-09-18 05:22:57 +0300

    mail now work it's ok

--- hello.py
+++ hello.py
@@ -1,76 +1,90 @@
 import os
-from flask import Flask,render_template,session,redirect,url_for,flash
-from flask.ext.mail import Mail
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script import Manager,Shell
-from flask.ext.migrate import Migrate,MigrateCommand
+from flask import Flask, render_template, session, redirect, url_for
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
-from datetime import datetime
 from flask.ext.wtf import Form
-from wtforms import StringField,SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.mail import Mail, Message
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY']="GOODtime54545sososolion"
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
-
-db=SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = "cakephp.php@gmail.com"
+#app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_PASSWORD'] = "zfvluxtohgbhloew"
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <cakephp.php@gmail.com>'
+#app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['FLASKY_ADMIN'] ="cakephp.php@gmail.com"
 
 manager = Manager(app)
-migrate = Migrate(app,db)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 mail = Mail(app)
 
-#config mail 
-app.config['MAIL_SERVER']='smtp.googlemail.com'
-app.config['MAIL_PORT']= 587
-app.config['MAIL_USE_TLS']= True
 
-#app.config['MAIL_USERNAME']= os.environ.get('MAIL_USERNAME')
-app.config['MAIL_USERNAME']= 'cakephp.php@gmail.com'
-#app.config['MAIL_PASSWORD']= os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_PASSWORD'] = "xfdumhcblujipbjd"
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 def make_shell_context():
-	return dict(app=app,db=db,User=User,Role=Role)
-
+    return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 
-class NameForm(Form):
-	name=StringField('What is your name?',validators=[Required()])
-	submit = SubmitField('Submit')
-		
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-class Role(db.Model):
-	__tablename__='roles'
-	id = db.Column(db.Integer,primary_key=True)
-	name=db.Column(db.String(64),unique=True)
-	users = db.relationship('User',backref='role',lazy='dynamic')
-	def __repr__(self):
-		return '<Role %r>'% self.name
-	
-class User(db.Model):
-	__tablename__='users'
-	id = db.Column(db.Integer,primary_key=True)
-	username =db.Column(db.String(64),unique=True,index=True)
-	role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-	def __repr__(self):
-		return '<User %r>'% self.username
-		
 
-		
-		
-		
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -80,27 +94,16 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False))
-						   
-		
-	
-@app.route('/user/<name>')
-def user(name):
-	return render_template("user.html",name = name)
 
-#custm error pages 
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html'),404
 
-@app.errorhandler(500)
-def internal_server_error(e):
-	return render_template('500.html'),500
-	
-if __name__ == "__main__":
-	manager.run()
+if __name__ == '__main__':
+    manager.run()
