6c17215a9b2710be2c0539320c1fa08d6826aaa2
playgoods <mohmed_e@hotmail.com>
2015-09-29 06:33:29 +0300

    fixed model and add file test_user_model.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -24,7 +25,7 @@ class User(db.Model):
 	
 	@password.setter
 	def password(self,password):
-		self.password_hash = gererate_password_hash(password)
+		self.password_hash = generate_password_hash(password)
 		
 	def verify_password(self,password):
 		return check_password_hash(self.password_hash,password)
