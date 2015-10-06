8629bd821b1f6d57e2e4b4cb3e1b8da4a2d5b5f8
playgoods <mohmed_e@hotmail.com>
2015-10-06 05:38:33 +0300

    auth and confirm by email

--- app/email.py
+++ app/email.py
@@ -9,6 +9,7 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 
+
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
