b5659c90bb1b60adb3c1cec34170ea387db64809
playgoods <mohmed_e@hotmail.com>
2015-10-14 21:59:08 +0300

    Role but not complate

--- app/models.py
+++ app/models.py
@@ -9,18 +9,50 @@ from . import login_manager
 def load_user(user_id):
 	return User.query.get(int(user_id))
 
+class Permission:
+	FOLLOW  = 0X01
+	COMMENT = 0X02
+	WRITE_ARTICLES = 0X04
+	MODERATE_COMMENTS = 0X08
+	ADMINISTER = 0X80
 
 class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	default = db.Column(db.Boolean,default=False,index=True)
+	permissions = db.Column(db.Integer)
+	users = db.relationship('User', backref='role', lazy='dynamic')
+	
+	@staticmethod
+	def insert_roles():
+		roles= {
+			'User':(Permission.FOLLOW|
+					Permission.COMMENT|
+					Permission.WRITE_ARTICLES,True),
+			'Moderator':(Permission.FOLLOW|
+						Permission.COMMENT |
+						Permission.WRITE_ARTICLES|
+						Permission.MODERATE_COMMENTS,False),
+			'Administrator':(0xff,False)
+			}
+			
+		for r in roles:
+			role = Role.query.filter_by(name=r).first()
+			if role is None:
+				role = Role(name=r)
+			role.permissions = roles[r][0]
+			role.default = roles[r][1]
+			db.session.add(role)
+		db.session.commit()
+			
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+	def __repr__(self):
+		return '<Role %r>' % self.name
 
 
 class User(UserMixin,db.Model):
+
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(64),unique=True,index=True)
@@ -28,6 +60,14 @@ class User(UserMixin,db.Model):
 	password_hash = db.Column(db.String(128))
 	confirmed = db.Column(db.Boolean, default=False )
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	
+	def __init__(self,**kwargs):
+		super(User,self).__init__(**kwargs)
+		if self.role is None:
+			if self.email == current_app.config['FLASK_ADMIN']:
+				self.role = Role.query.filter_by(permissions=0xff).first()
+			if self.role is None:
+				self.role = Role.query.filter_by(default=True).first()
 	
 	@property
 	def password(self):
