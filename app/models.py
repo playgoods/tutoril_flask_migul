27d0db6dc3b44331c45f7f3d1e564bcdf5623418
playgoods <mohmed_e@hotmail.com>
2015-10-15 22:00:48 +0300

    first toturail user profile part 1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -60,6 +61,16 @@ class User(UserMixin,db.Model):
 	password_hash = db.Column(db.String(128))
 	confirmed = db.Column(db.Boolean, default=False )
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	name = db.Column(db.String(64))
+	location = db.Column(db.String(64))
+	about_me = db.Column(db.Text())
+	member_since = db.Column(db.DateTime(),default=datetime.utcnow)
+	last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
+	
+	def ping(self):
+		self.last_seen = datetime.utcnow()
+		db.session.add(self)
+		
 	
 	def __init__(self,**kwargs):
 		super(User,self).__init__(**kwargs)
