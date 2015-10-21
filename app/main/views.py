d4f64fa02da2189cc62990ef9c23fce2c21d6ae3
playgoods <mohmed_e@hotmail.com>
2015-10-21 21:27:41 +0300

    contune post add pagangiton

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template,session,redirect,url_for,current_app,flash
+from flask import render_template,session,redirect,url_for,current_app,flash,request
 from flask.ext.login import login_required,current_user
 from ..decorators import admin_required
 from . import main
@@ -18,8 +18,11 @@ def index():
 				author = current_user._get_current_object())
 		db.session.add(post)
 		return redirect(url_for('.index'))
-	posts = Post.query.order_by(Post.timestamp.desc()).all()
-	return render_template('index.html',form=form,posts=posts)
+	page = request.args.get('page',1,type=int)
+	pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+		page,per_page=current_app.config['FLASK_POSTS_PER_PAGE'])
+	posts = pagination.items
+	return render_template('index.html',form=form,posts=posts ,pagination = pagination)
 	
 			   
 @main.route('/user/<username>')
