785d33137e529f8e1a80b6effb76dd73a89a17dc
playgoods <mohmed_e@hotmail.com>
2015-10-26 20:52:09 +0300

    add table follow

--- app/models.py
+++ app/models.py
@@ -54,7 +54,14 @@ class Role(db.Model):
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
-
+		
+class Follow(db.Model):
+	__tablename__ = 'follows'
+	follower_id = db.Column(db.Integer,db.ForeignKey('users.id'),
+							primary_key = True)
+	followed_id = db.Column(db.Integer,db.ForeignKey('users.id'),
+							primary_key = True)
+	timestamp = db.Column(db.DateTime,default=datetime.utcnow)
 
 class User(UserMixin,db.Model):
 
@@ -195,6 +202,39 @@ class User(UserMixin,db.Model):
 			self.email.encode('utf-8')).hexdigest()
 		db.session.add(self)
 		return True
+		
+	followed = db.relationship('Follow',
+								foreign_keys =[Follow.follower_id],
+								backref=db.backref('follower',lazy='joined'),
+								lazy='dynamic',
+								cascade='all,delete-orphan')
+	followers = db.relationship('Follow',
+								foreign_keys =[Follow.followed_id],
+								backref=db.backref('followed',lazy='joined'),
+								lazy='dynamic',
+								cascade='all,delete-orphan')
+	
+	def follow(self,user):
+		if not self.is_following(user):
+			f = Follow(follower=self,followed=user)
+			db.session.add(f)
+			
+	def unfollow(self,user):
+		f=self.followed.filter_by(followed_id=user.id).first()
+		if f:
+			db.session.delete(f)
+	
+	def is_following(self,user):
+		return self.followed.filter_by(
+			followed_id=user.id).first() is not None
+			
+	def is_followed_by(self,user):
+		return self.followers.filter_by(
+			follower_id=user.id).first() is not None
+			
+
+			
+			
 
 	def __repr__(self): 
 		return '<User %r>' % self.username
