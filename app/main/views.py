b7438c11ef7c9e4c9a0082030ec80dca65a6f6c9
playgoods <mohmed_e@hotmail.com>
2015-11-10 02:09:06 +0300

    the last test with error on API test

--- app/main/views.py
+++ app/main/views.py
@@ -246,6 +246,15 @@ def moderate_disable(id):
 							page=request.args.get('page',1,type=int)))
 								
 	
+@main.route('/shutdown')	
+def server_shutdown():
+	if not current_app.testing:
+		abort(404)
+	shutdown = request.environ.get('werkzeug.server.shutdown')
+	if not shutdown:
+		abort(500)
+	shutdown()
+	return 'Shutting down...................'
 	
 
 	
